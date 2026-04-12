d9acc85fa685fe4ac0a42a84ba47f1f9586e93ba
John-Grace <johnsimwerayi@gmail.com>
2026-04-13 04:52:30 +0800

    Minor tweak in login page

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -17,26 +17,25 @@ const Login = () => {
 	};
 
 	const handleSignUp = async () => {
-		return
+		return;
 	};
 	return (
 		<div className='flex gap-12 justify-center'>
-					<div className='flex flex-col items-center gap-12 py-8 h-full w-[35%]'>
-						<h1 className='font-bold text-2xl mr-18'>
-							Welcome to MU's #1 food-ordering app
-						</h1>
-						
-						<p>
-							Your go-to app for ordering from MU's various canteens for pickup or
-							delivery. Sign in and look for suggested dishes, or head over to the
-							Restaurants tab for more
-						</p>
-						<div className='h-[80%]'></div>
-					</div>
-					<div className='bg-(--secondary-colour) w-[35%] h-[420px] px-12 py-4 rounded-3xl flex flex-col items-start justify-center'>
-					<div className='flex flex-col gap-8 w-[80%] items-center'>
+			<div className='flex flex-col items-center gap-12 py-8 h-full w-[35%]'>
+				<h1 className='font-bold text-2xl mr-18'>
+					Welcome to MU's #1 food-ordering app
+				</h1>
+
+				<p>
+					Your go-to app for ordering from MU's various canteens for pickup or
+					delivery. Sign in and look for suggested dishes, or head over to the
+					Restaurants tab for more
+				</p>
+				<div className='h-[80%]'></div>
+			</div>
+			<div className='bg-(--secondary-colour) w-[35%] h-[420px] px-12 py-4 rounded-3xl flex flex-col items-start justify-center'>
+				<div className='flex flex-col gap-8 w-[80%] items-center'>
 					<div className='w-full flex gap-2 py-3 px-4 cursor-pointer whitespace-nowrap'>
-						
 						<div className='bg-white w-[100%] h-12 rounded-3xl p-2 pl-12 text-sm flex gap-2 items-center'>
 							<input
 								className='outline-none border-none bg-transparent w-full'
@@ -51,7 +50,7 @@ const Login = () => {
 					<div className='w-full flex gap-2 py-3 px-4 cursor-pointer whitespace-nowrap'>
 						<div className='bg-white w-[100%] h-12 rounded-3xl p-2 pl-12 text-sm flex gap-2 items-center'>
 							<input
-							className='outline-none border-none bg-transparent w-full'
+								className='outline-none border-none bg-transparent w-full'
 								type='password'
 								name='password'
 								placeholder='Password'
@@ -61,23 +60,19 @@ const Login = () => {
 						</div>
 					</div>
 					<div className='flex justify-between'>
-						<button className='bg-(--primary-colour) text-black mt-6 w-40 h-12 ml-6 rounded-3xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200 ease-in-out'>
-							<Link
-								className='text-black px-4 py-2 rounded-md cursor-pointer'
-								href={'/home'}>
-								Sign In
-							</Link>
-						</button>
-						<button className='bg-(--primary-colour) text-black ml-12 mt-6 w-40 h-12 rounded-3xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200 ease-in-out'>
 						<Link
-							className='text-black px-4 py-2 rounded-md cursor-pointer'
+							className='bg-(--primary-colour) text-(--text-colour) mt-6 w-40 h-12 ml-6 rounded-3xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200 ease-in-out'
+							href={'/home'}>
+							Sign In
+						</Link>
+						<Link
+							className='bg-(--primary-colour) text-(--text-colour) mt-6 w-40 h-12 ml-6 rounded-3xl cursor-pointer hover:scale-105 active:scale-95 transition-all duration-200 ease-in-out'
 							href={'/home'}>
 							Sign Up
 						</Link>
-					</button>
-					</div>
 					</div>
 				</div>
+			</div>
 		</div>
 	);
 };
